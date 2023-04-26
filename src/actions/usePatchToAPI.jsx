c1ba9d687c1105a9564a79a6a87114facce7b8b1
seu nome <adnanbezerra@gmail.com>
2023-04-26 20:39:35 -0300

    feat: :sparkles: create patch to api hook

---
+++ src/actions/usePatchToAPI.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from '../mock/apiUrl';
+
+function usePatchFromAPI(objectId, requestData) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(async () => {
+    setLoading(true);
+
+    axios.post(`${API_URL}/carrers/${objectId}/`, requestData)
+      .then(reponse => {
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+      });
+
+  }, [requestData]);
+
+  return { loading, error };
+}
+
+export default usePatchFromAPI;
