2e02eb366f223aed155dba3b6484be5d9c94d9fb
seu nome <adnanbezerra@gmail.com>
2023-04-27 20:41:57 -0300

    feat: :sparkles: add posts into main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,33 @@
-import { useEffect, useState } from "react";
 import NewPostForm from "../../components/NewPostForm.jsx/NewPostForm";
 import { Container, Content, Title, TitleDiv } from "./MainPageStyles";
 import useGetFromAPI from "../../actions/useGetFromAPI";
+import Post from "../../components/Post/Post";
+import { useEffect, useState } from "react";
 
 export default function MainPage() {
-    return(
+    const { data } = useGetFromAPI();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        if (JSON.stringify(data) !== JSON.stringify([{}])) {
+            setPosts(data.results)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data]);
+
+    function getPosts() {
+        return (
+            posts.map(entry => {
+                return (
+                    <Post
+                        key={entry.id}
+                    />
+                )
+            })
+        )
+    }
+
+    return (
         <Container>
             <Content>
                 <TitleDiv>
@@ -12,6 +35,8 @@ export default function MainPage() {
                 </TitleDiv>
 
                 <NewPostForm />
+
+                {getPosts()}
             </Content>
         </Container>
     )
